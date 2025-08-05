d268e39c029d439baaa91e9e644223436503fbb0
sradostina <sofii@LAPTOPSONYA2>
2025-08-05 13:21:29 +0300

    client part

---
+++ dialog.h
@@ -0,0 +1,33 @@
+#ifndef DIALOG_H
+#define DIALOG_H
+
+#include <QDialog>
+#include <QTcpSocket>
+
+namespace Ui {
+class Dialog;
+}
+
+class Dialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit Dialog(QWidget *parent = nullptr);
+    ~Dialog();
+
+signals:
+    void loginSuccess(const QString &username);
+    void registrationSuccess();
+
+private slots:
+    void on_sign_clicked();
+    void on_create_clicked();
+
+private:
+    Ui::Dialog *ui;
+    QTcpSocket *socket;
+    void sendRequest(const QJsonObject &request);
+};
+
+#endif // DIALOG_H
